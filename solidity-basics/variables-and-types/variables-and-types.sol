7312b70af86d49d95a8bbbe69312d95e337473a5
0xShravanth <shravanth0023@gmail.com>
2025-06-16 00:31:13 +0530

    added variable and types

---
+++ solidity-basics/variables-and-types/variables-and-types.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+// variables are used to store information - they are the memory location nwe store value
+
+// types: boolean - return true or false : eg: liedetector
+// integer - keyword uint -value signed and unsigned integers eg: walletamount
+// string  - keyword string- data value that made up of sequence of character eg: errormessage
+
+
+// contract Test { // the contract is created and called test
+//     address public owner;   // creates a new variable called owner
+//     string public name = "CryptoDevs";      //creates a string variable and sets it's value to "CryptoDevs" 
+//     uint public totalSupply = 10000 * 10 ** 18;     // creates a new variable called totalSupply and assigns it the value of 10000 times 10 raised to the power of 18
+
+
+//     constructor() public {       //creates a constructor
+//         owner = msg.sender;      //
+
+contract learnVariable{
+    uint chocolateBar = 10;
+    uint public storeOwner = 300;
+    bool lieDetector = true;
+    string errorMessageText = "Error! There has be an mistake"; // use single cotation(') or double cotation("")
+    
+}
