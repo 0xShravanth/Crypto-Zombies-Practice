61740331e1405155cd466c065b35cee61ea17309
0xShravanth <shravanth0023@gmail.com>
2025-07-22 22:16:58 +0530

    added PausableToken:pause()/unpause(),whenNotPaused,mint(),burn(),Events CappedToken:immutable _cap, mint(), cap(), _mint() TimelockToken:lockTokens(),notLocked(),_lockUntil[address], getUnlockTime()

--- Contract Conqueror/Erc-20/ERC20standard.sol
+++ Contract Conqueror/Erc-20/ERC20standard.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 ///@title Contract Conqueror ERC20 STandard 
 
-contract BasicToken {
+contract ERC20Token {
     /// @notice Token details
     string public name;
     string public symbol;
@@ -21,14 +21,17 @@ contract BasicToken {
     mapping (address => mapping(address => uint256)) private _allowance;
 
     // @dev Timestamp when each account's tokens unlock
+    mapping(address => uint256 ) private _lockUntil;
 
     /// @notice Events for tracking state changes
+
     event Transfer(address indexed from, address indexed to , uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
     event Mint(address indexed to, uint256 amount);
     event Burn(address indexed from, uint256 amount);
     event Paused(address indexed  account);
-     event Unpaused(address indexed  account);
+    event Unpaused(address indexed  account);
+    event Locked(address indexed user, uint256 amount);
     /// @notice Events for tracking state changes
 
     /// @dev Restrict function to only the owner
@@ -44,23 +47,31 @@ contract BasicToken {
     }
 
     /// @dev Restrict function if address is time-locked
+    modifier  notLocked(address user) {
+        require(block.timestamp >= _lockUntil[user],"Token are time-locked");
+        _;
+    }
 
     /// @notice Constructor to initialize name, symbol, cap, and initial supply
-    // / @param _name Token name
-    // / @param _symbol Token symbol
-    // / @param initialSupply Initial mint amount (in whole tokens)
-    // / @param maxCap Maximum supply cap (in whole tokens)
-    constructor(string memory _name, string memory _symbol,uint256 initialSupply){
+    /// @param _name Token name
+    /// @param _symbol Token symbol
+    /// @param initialSupply Initial mint amount (in whole tokens)
+    /// @param maxCap Maximum supply cap (in whole tokens)
+    constructor(string memory _name, string memory _symbol,uint256 initialSupply, uint256 maxCap){
+        require(maxCap >= initialSupply, "cap should be >= initial supply");
         name = _name;
         symbol = _symbol;
         owner = msg.sender;
+        _cap = maxCap * (10 ** uint256(decimals));
         _mint(owner,initialSupply * (10 ** uint256(decimals)));
     }
+
     /// @notice Pause token transfers and actions
     function pause() external onlyOwner{
         paused = true;
         emit Paused(msg.sender);
     }
+
     /// @notice Unpause token transfers and actions
     function unPause() external onlyOwner{
         paused = false;
@@ -70,22 +81,35 @@ contract BasicToken {
     /// @notice Lock an address's tokens until a future time
     /// @param user The address to lock
     /// @param unlockTime The UNIX timestamp until which the address is locked    
+    function lockTokens(address user, uint256 unlockTime) external onlyOwner{
+        uint256  currentunlockTime = block.timestamp + unlockTime;
+        require(currentunlockTime > block.timestamp, "unlock time must be in future");
+        _lockUntil[user] = currentunlockTime;
+        emit Locked(user, currentunlockTime);
+    }
 
     /// @notice Get the unlock timestamp for a user
-
+    function getUnlockTime(address user) external view returns (uint256) {
+        return _lockUntil[user];
+    }
+    /// @notice Get the block timestamp 
+    function getCurrentBlockTimestamp() external view returns (uint256) {
+        return block.timestamp;
+    }
+    
     /// @notice Mint new tokens (only owner, capped)
     /// @param to The address to receive minted tokens
     /// @param amount The amount to mint (in whole tokens)
     function mint(address to, uint256 amount)external onlyOwner whenNotPaused{
-         uint256 amountWithDecimals = amount * (10 ** uint256(decimals));
-        // require(_totalSupply + amountWithDecimals <= _cap, "Exceeds cap"); 
+        uint256 amountWithDecimals = amount * (10 ** uint256(decimals));
+        require(_totalSupply + amountWithDecimals <= _cap, "Exceeds cap"); 
        _mint(to, amountWithDecimals);
     }
     
 
     /// @notice Burn tokens from caller's balance
     /// @param amount Amount to burn (in smallest unit)
-    function burn( uint256 amount)external whenNotPaused{
+    function burn( uint256 amount)external notLocked(msg.sender) whenNotPaused{
         require(_balances[msg.sender] >= amount ,"Insufficient balance");
         _burn(msg.sender, amount);
     }
@@ -93,43 +117,52 @@ contract BasicToken {
     /// @notice Burn tokens from another account with approval
     /// @param account Address to burn from
     /// @param amount Amount to burn (in smallest unit)
-    function burnFrom(address account, uint256 amount)external whenNotPaused {
+    function burnFrom(address account, uint256 amount)external notLocked(msg.sender) whenNotPaused {
         require(_allowance[account][msg.sender] >= amount,"Allowance exceeded");
         _allowance[account][msg.sender] -= amount;
         _burn(account, amount);
     }   
 
     /// @notice Transfer tokens
-    function transfer(address to, uint256 amount) external whenNotPaused returns(bool) {
+    function transfer(address to, uint256 amount) external whenNotPaused notLocked(msg.sender) returns(bool) {
         _transfer(msg.sender, to, amount);
         return true;
-        }
+    }
+
     /// @notice Approve a spender
-    function approve(address spender, uint256 amount) external whenNotPaused returns(bool){
+    function approve(address spender, uint256 amount) external whenNotPaused notLocked(msg.sender) returns(bool){
         _approve(msg.sender, spender,  amount);
         return true;
     }
+
     /// @notice Transfer tokens using allowance
-    function transferFrom(address from, address to, uint256 amount) external whenNotPaused returns(bool){
-        require(_allowance[from][msg.sender]>= amount, "insufficient balance");
+    function transferFrom(address from, address to, uint256 amount) external whenNotPaused notLocked(from) returns(bool){
+        require(_allowance[from][msg.sender] >= amount, "insufficient Allowance  balance");
         _allowance[from][msg.sender] -= amount;
         _transfer(from, to, amount);
         return true;
     }
 
     /// @notice Get max token cap
+    function cap() external view returns (uint256){
+        return _cap;
+    }
+
     /// @notice Get total token supply
     function totalsupply() external view returns (uint256){
         return _totalSupply;
     }
+
     /// @notice Get balance of an address
     function balanceOf(address account) external view returns (uint256) {
         return  _balances[account];
     }
+
     /// @notice Get allowance from owner to spender
     function allowance(address tokenOwner, address spender) external view returns(uint256){
         return _allowance[tokenOwner][spender];
     }
+
     /// @dev Internal mint function (only callable in constructor here)
     function _mint(address to, uint256 amount) internal {
         require(to != address(0),"Invalid address");
@@ -138,6 +171,7 @@ contract BasicToken {
         emit Mint(to, amount);
         emit Transfer(address(0), to, amount);
     }
+
     /// @dev Internal burn function
     function _burn(address from , uint256 amount) internal {
         require(from != address(0), "burn from 0x0");
@@ -146,8 +180,8 @@ contract BasicToken {
         _totalSupply -= amount;
         emit Burn(from, amount);
         emit Transfer(from, address(0), amount);
-
     }
+
     /// @dev Internal transfer function
     function _transfer(address from, address to, uint256 amount) internal {
         require(to != address(0), "Invalid address");
@@ -158,6 +192,7 @@ contract BasicToken {
         _balances[to] += amount;
         emit Transfer(from, to, amount);
     }
+
     /// @dev Internal approve function
     function _approve(address tokenOwner, address spender, uint256 amount)internal{
         require(tokenOwner != address(0), "ERC20: approve from 0x0");
